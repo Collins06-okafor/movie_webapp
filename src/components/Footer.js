edcd1e874c14cd4b2ba10e3c22382fb149cf2fa1
Collins06-okafor <chuwubuikem.okafor@final.edu.tr>
2025-06-27 08:39:11 +0300

    fotter made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="footer">
-    <p style={{ textAlign: 'center', color: '#fff', marginTop: '2rem' }}>
+    <p style={{ textAlign: 'center', color: '#fff', marginTop: '0rem', marginBottom: '0rem' }}>
       © 2025 Chubby Okafor | chubby@example.com
     </p>
   </footer>
